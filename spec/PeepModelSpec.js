d8f2c545503dc0a832d8f152e20c211d2e3b8cce
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-23 18:27:17 +0000

    Peep can display author handle

--- spec/PeepModelSpec.js
+++ spec/PeepModelSpec.js
@@ -2,11 +2,11 @@ describe("Peep Model", function () {
     var testpeep;
 
     beforeEach( function() {
-        testpeep = new Peep (495, body = "what is dead may never die",
+        testpeep = new Peep (id = 495, body = "what is dead may never die",
         created_at = "2019-02-10T20:15:14.723Z",
         updated_at = "2019-02-10T20:15:14.723Z",
         user_id = 774,
-        user_handle = "fff");
+        user_handle = "fff", likes = 0);
     });
 
     it("should be able to initialize a new peep model", function() {
@@ -20,5 +20,13 @@ describe("Peep Model", function () {
         expect(testpeep.updated_at).toContain("2019-02-10")
         expect(testpeep.user_id).toEqual(774)
         expect(testpeep.user_handle).toEqual("fff")
+    })
+
+    it("has a method for displaying the Peep body", function () {
+        expect(testpeep.getPeepBody()).toEqual(testpeep.body)
+    })
+
+    it("has a method for displaying the Peep author's user handle", function () {
+        expect(testpeep.getPeepAuthorHandle()).toEqual(testpeep.user_handle)
     })
 });
--- src/PeepModel.js
+++ src/PeepModel.js
@@ -1,14 +1,20 @@
 (function (exports) {
-    function Peep(id, body, created_at, updated_at, user_id, user_handle){
+    function Peep(id, body, created_at, updated_at, user_id, user_handle, likes){
         this.id = id
         this.body = body
         this.created_at = created_at
         this.updated_at = updated_at
         this.user_id = user_id
         this.user_handle = user_handle
+        this.likes = likes
     }
-    // Peep.prototype. = function () {
-    //     return this._noteText;
-    // };
+    Peep.prototype.getPeepBody = function () {
+        return this.body;
+    }
+
+    Peep.prototype.getPeepAuthorHandle = function () {
+        return this.user_handle
+    }
+
     exports.Peep = Peep
 })(this);
