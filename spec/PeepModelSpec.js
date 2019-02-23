2d158e407225e9ce734b22b890741013d9618a78
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-23 16:24:31 +0000

    Peeps have parameters based on what the API returns

--- spec/PeepModelSpec.js
+++ spec/PeepModelSpec.js
@@ -1,10 +1,24 @@
 describe("Peep Model", function () {
+    var testpeep;
 
     beforeEach( function() {
+        testpeep = new Peep (495, body = "what is dead may never die",
+        created_at = "2019-02-10T20:15:14.723Z",
+        updated_at = "2019-02-10T20:15:14.723Z",
+        user_id = 774,
+        user_handle = "fff");
     });
 
     it("should be able to initialize a new peep model", function() {
-        var testpeep = new Peep();
         expect(testpeep instanceof Peep).toBe(true)
+    })
+
+    it("has parameters id, body, created_at, updated_at, user(id, handle)", function () {
+        expect(testpeep.id).toEqual(495)
+        expect(testpeep.body).toContain("what is dead")
+        expect(testpeep.created_at).toContain("2019-02-10")
+        expect(testpeep.updated_at).toContain("2019-02-10")
+        expect(testpeep.user_id).toEqual(774)
+        expect(testpeep.user_handle).toEqual("fff")
     })
 });
--- src/PeepModel.js
+++ src/PeepModel.js
@@ -1,6 +1,11 @@
 (function (exports) {
-    function Peep(){
-
+    function Peep(id, body, created_at, updated_at, user_id, user_handle){
+        this.id = id
+        this.body = body
+        this.created_at = created_at
+        this.updated_at = updated_at
+        this.user_id = user_id
+        this.user_handle = user_handle
     }
     // Peep.prototype. = function () {
     //     return this._noteText;
